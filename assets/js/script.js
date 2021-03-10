584ee631c2095eb76ec268f2f048d839e3c6f52b
Vittokm <vittokm@gmail.com>
2021-03-10 18:54:43 +0100

    fixed problem with input submit
    because the button was of  a submit type, it was doing a reload of the page every time i pressed it. After changing the type to button, everything worked fine.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,15 @@
-var i = 1;
+var num = 1;
 function add() {
-
+    
     let name = document.querySelector("#name");    
     let date = document.querySelector("#date");
     let amount = document.querySelector("#amount");
 
-    let tabela = document.querySelector("#tabela");
-    let row = tabela.insertRow(i);
+    let tbl = document.querySelector("#tbl");
+
+    tbl.style.display = "flex";
+
+    let row = tbl.insertRow(num);
     let cell1 = row.insertCell(0);
     let cell2 = row.insertCell(1);
     let cell3 = row.insertCell(2);
@@ -16,7 +19,23 @@ function add() {
     cell2.innerHTML = date.value;
     cell3.innerHTML = amount.value;
     cell4.innerHTML = "<button onclick='del(this)'> X </button>";
-
-    i++;
     
+    name.value = "";
+    date.value = "";
+    amount.value = "";
+
+    num++;
+}
+
+function del() {
+    
+    let del = document.querySelector("#tbl");
+    num--;
+
+    if (num <= 1) {
+        del.querySelector("td").parentElement.remove();
+        del.style.display = "none";
+    } else {
+        del.querySelector("td").parentElement.remove();
+    }
 }
